c7fbd6b14db7583fb999a15223f718289d7da2f2
WillOli <william.ika@gmail.com>
2024-01-25 17:27:59 -0300

    feat(formulario): adicionando formulário JForm

---
+++ prjinfox/src/screen/TelaLogin.java
@@ -0,0 +1,11 @@
+package screen;
+
+import javax.swing.*;
+
+public class TelaLogin {
+    private JPanel systemX;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+
+}
